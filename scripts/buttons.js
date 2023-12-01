c2d8fb9d520b78ad4e9fb5712e777235761c4bfe
kris-den-hertog <122313839+kris-den-hertog@users.noreply.github.com>
2023-12-01 11:45:47 +0100

    test

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -1,8 +1,22 @@
 const FAQ = document.getElementById("FAQ");
+const BBL_B = document.getElementById("BBL_Button");
+const BOL_B = document.getElementById("BOL_Button");
 
 FAQ.addEventListener("click", open_FAQ) 
 
 function open_FAQ() {
     window.location.href = "../pages/faq.html";
+}
+
+BBL_B.addEventListener("click", open_BBL) 
+
+function open_BBL() {
+    window.location.href = "../pages/BBL.html";
+}
+
+BOL_B.addEventListener("click", open_BOL) 
+
+function open_BOL() {
+    window.location.href = "../pages/BOL.html";
 }
   
